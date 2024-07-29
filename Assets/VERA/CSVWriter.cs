3066ca19ab0fd36b2855c7b570228c04c7876804
lucidbard <lucidbard@users.github.com>
2024-07-29 17:32:27 +0000

    Jul 29, 2024, 1:32 PM

--- Assets/VERA/CSVWriter.cs
+++ Assets/VERA/CSVWriter.cs
@@ -1,18 +1,126 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class CSVWriter : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private static string filePath;
+    private static List<Column> columns = new List<Column>();
+    public string study_UUID;
+
+    [Serializable]
+    public class Column
     {
-        
+        public string name;
+        public DataType type;
+
+        public Column(string name, DataType type)
+        {
+            this.name = name;
+            this.type = type;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public enum DataType
     {
-        
+        Number,
+        String,
+        JSON
+    }
+
+    // Saves the CSV file to the application directory.
+    public void SaveCSV()
+    {
+        string path = Path.Combine(Application.persistentDataPath, study_UUID + ".csv");
+        File.Move(filePath, path);
+    }
+
+    // Sends the CSV to the server via a post request
+    public void SubmitCSV()
+    {
+        string path = Path.Combine(Application.persistentDataPath, study_UUID + ".csv");
+        string url = "http://sherlock.gaim.ucf.edu/api/" + study_UUID + "/testudid";
+        WWWForm form = new WWWForm();
+        form.AddField("study_UUID", study_UUID);
+        form.AddBinaryData("file", File.ReadAllBytes(path), study_UUID + ".csv", "text/csv");
+        WWW www = new WWW(url, form);
+        StartCoroutine(WaitForRequest(www));
+    }
+
+    private IEnumerator WaitForRequest(WWW www)
+    {
+        yield return www;
+
+        // check for errors
+        if (www.error == null)
+        {
+            Debug.Log("WWW Ok!: " + www.text);
+        }
+        else
+        {
+            Debug.Log("WWW Error: " + www.error);
+        }
+    }
+
+    // Initialize the CSV file with columns
+    public static void Initialize(string fileName, ArrayList columnDefinitions)
+    {
+        filePath = Path.Combine(Application.persistentDataPath, fileName);
+
+        using (StreamWriter writer = new StreamWriter(filePath))
+        {
+            columns.Clear();
+            List<string> columnNames = new List<string>();
+
+            foreach (object obj in columnDefinitions)
+            {
+                Column column = (Column)obj;
+                columns.Add(column);
+                columnNames.Add(column.name);
+            }
+
+            writer.WriteLine(string.Join(",", columnNames));
+        }
+    }
+
+    // Create a new entry in the CSV file
+    public static void CreateEntry(params object[] values)
+    {
+        if (values.Length != columns.Count)
+        {
+            Debug.LogError("Values count does not match columns count.");
+            return;
+        }
+
+        List<string> entry = new List<string>();
+
+        for (int i = 0; i < values.Length; i++)
+        {
+            object value = values[i];
+            Column column = columns[i];
+
+            switch (column.type)
+            {
+                case DataType.Number:
+                    entry.Add(Convert.ToString(value));
+                    break;
+                case DataType.String:
+                    entry.Add($"\"{value.ToString()}\"");
+                    break;
+                case DataType.JSON:
+                    entry.Add($"\"{JsonUtility.ToJson(value)}\"");
+                    break;
+                default:
+                    entry.Add(value.ToString());
+                    break;
+            }
+        }
+
+        using (StreamWriter writer = new StreamWriter(filePath, true))
+        {
+            writer.WriteLine(string.Join(",", entry));
+        }
     }
 }

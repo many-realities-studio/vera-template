64cd87cfeedfe9a4ae5d109d6507c7a6b41103b8
lucidbard <lucidbard@users.github.com>
2024-07-29 17:53:29 +0000

    Jul 29, 2024, 1:53 PM

--- Assets/VERA/CSVWriter.cs
+++ Assets/VERA/CSVWriter.cs
@@ -74,7 +74,7 @@ public class CSVWriter : MonoBehaviour
     }
 
     // Initialize the CSV file with columns
-    public void Initialize(ArrayList columnDefinitions)
+    public void Initialize(List<Column> columnDefinitions)
     {
         filePath = Path.Combine(Application.persistentDataPath, study_UUID+"-"+participant_UUID + ".csv");
 
